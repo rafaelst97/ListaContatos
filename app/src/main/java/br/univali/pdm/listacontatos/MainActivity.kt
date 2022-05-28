fe2e2d329d40a1aaa33fe6f953b6f4598cf7a443
Rafael dos Santos <rafaels1997@LIVE.COM>
2022-05-28 14:56:45 -0300

    AdicionarContatoFragment.kt criada

--- app/src/main/java/br/univali/pdm/listacontatos/MainActivity.kt
+++ app/src/main/java/br/univali/pdm/listacontatos/MainActivity.kt
@@ -2,25 +2,42 @@ package br.univali.pdm.listacontatos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
-    private lateinit var adicionarContato: Button
+    private lateinit var adicionarContato: FloatingActionButton
 
     private lateinit var listaContatosFragment: ListaContatosFragment
+    private lateinit var adicionarContatoFragment: AdicionarContatoFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         adicionarContato = findViewById(R.id.adicionarContato)
+        adicionarContato.setOnClickListener(this)
 
         listaContatosFragment = ListaContatosFragment()
+        adicionarContatoFragment = AdicionarContatoFragment()
     }
 
-    private fun setFragment(fragment: ListaContatosFragment){
+    private fun setFragment(fragment: AdicionarContatoFragment){
         val fragmentTransition = supportFragmentManager.beginTransaction()
+        fragmentTransition.replace(R.id.fragmentContainerView, fragment)
+        fragmentTransition.commit()
+    }
+
+    override fun onClick(v: View) {
+
+        when (v.id){
+            R.id.adicionarContato -> {
+                setFragment(adicionarContatoFragment)
+            }
+        }
+
     }
 
 }
